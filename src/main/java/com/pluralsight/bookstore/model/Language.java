60c32a150eb077d19d098cdfd3c6a14818bcef53
sp <spatel2605@gmail.com>
2018-03-15 11:42:05 -0400

    inital code commit

---
+++ src/main/java/com/pluralsight/bookstore/model/Language.java
@@ -0,0 +1,10 @@
+package com.pluralsight.bookstore.model;
+
+public enum Language {
+
+    // ======================================
+    // =             Constants              =
+    // ======================================
+
+    ENGLISH, FRENCH, SPANISH, PORTUGUESE, ITALIAN, FINISH, GERMAN, DEUTSCH, RUSSIAN
+}
