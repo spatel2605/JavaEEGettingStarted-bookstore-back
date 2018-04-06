878d53cfec7349fe7492d85e4a583cc022af03eb
sp <spatel2605@gmail.com>
2018-04-06 13:09:35 -0400

    added rest api test

--- src/test/java/com/pluralsight/bookstore/rest/BookEndpointTest.java
+++ src/test/java/com/pluralsight/bookstore/rest/BookEndpointTest.java
@@ -8,6 +8,7 @@ import com.pluralsight.bookstore.util.NumberGenerator;
 import com.pluralsight.bookstore.util.TextUtil;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.extension.rest.client.ArquillianResteasyResource;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
@@ -16,7 +17,17 @@ import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
+
+import java.util.Date;
+
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static javax.ws.rs.core.Response.Status.CREATED;
+import static javax.ws.rs.core.Response.Status.NO_CONTENT;
+import static org.junit.Assert.assertEquals;
+
 
 @RunWith(Arquillian.class)
 @RunAsClient
@@ -40,6 +51,19 @@ public class BookEndpointTest {
     @Test
     public void createBook(@ArquillianResteasyResource("api/books")WebTarget webTarget) {
 
-        //Test
+        //Test couunting books
+        Response response = webTarget.path("count").request().get();
+        assertEquals(NO_CONTENT.getStatusCode(), response.getStatus());
+
+        //Test find all books
+        response = webTarget.request(APPLICATION_JSON).get();
+        assertEquals(NO_CONTENT.getStatusCode(), response.getStatus());
+
+        //Creates a book
+        Book book = new Book("isbn", "title", 12F, 123, Language.ENGLISH, new Date(), "imageURL", "description");
+        response = webTarget.request(APPLICATION_JSON).post(Entity.entity(book, APPLICATION_JSON));
+        assertEquals(CREATED.getStatusCode(), response.getStatus());
+
+
     }
 }
